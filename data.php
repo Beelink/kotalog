dc52ef8b65128e96666a8b9552d6bb63cabfd8c6
Slashxdd <32466372+Slashxdd@users.noreply.github.com>
2019-05-07 11:52:14 +0300

    Update data.php

--- data.php
+++ data.php
@@ -27,11 +27,11 @@
        echo '<li class="devices__item">
                 <div>
                     <img class="item-img" src="img/'.$category_id.'/'.$row['name'].' = '.$row['model'].'.png" alt="phone image">
-                    <h3 class="item-name">'.$row['name'].' '.$row['model'].' | '.$txt.'</h3>
-                    <p class="item-desc">'.$row['description'].'</p>
-                    <a href="" class="item-more">Подробнее</a>
-                    <input type="checkbox" name="compare" class="item-compare">
-                    <a href="" class="item-reviews">Отзывы</a>
+           <h3 class="item-name">'.$row['name'].' '.$row['model'].' | '.$txt.'</h3>
+           <p class="item-desc">'.$row['description'].'</p>
+           <a href="more.php?id='.$row['id'].'&category='.$category_id.'#device" class="item-more">Подробнее</a>
+           Сравнить <input type="checkbox" name="compare" class="item-compare">
+           <a href="more.php?id='.$row['id'].'&category='.$category_id.'#review" class="item-reviews">Отзывы</a>
                 </div>
             </li>';
       if($i >= $count) {
